5dfea9cf505e7906758505249538de6127194235
Игорь Насибов <nasibov.iv@yandex.ru>
2020-02-05 18:45:53 +0300

    Исправлены замечания

--- js/main.js
+++ js/main.js
@@ -134,9 +134,9 @@ var getInfoAdElement = function (element) {
   infoElement.querySelector('.popup__features').innerHTML = '';
 
   for (var i = 0; i < element.offer.features.length; i++) {
-    var createNewElement = document.createElement('li');
-    createNewElement.className = 'popup__feature popup__feature--' + element.offer.features[i];
-    infoElement.querySelector('.popup__features').appendChild(createNewElement);
+    var newElement = document.createElement('li');
+    newElement.className = 'popup__feature popup__feature--' + element.offer.features[i];
+    infoElement.querySelector('.popup__features').appendChild(newElement);
   }
 
   if (element.offer.description.length === 0) {
@@ -203,10 +203,10 @@ var setActiveCondition = function () {
   roomNumberElement.addEventListener('change', onRoomNumberSelectorChanged);
   checkinSelectElement.addEventListener('change', onCheckinTimeSelectorChanged);
   checkoutSelectElement.addEventListener('change', onCheckoutTimeSelectorChanged);
-  typeElement.addEventListener('change', onRoomTypeChangePrice);
+  typeElement.addEventListener('change', onRoomTypeChangePricePerNight);
 
-  mapPinsElements = mapPinsElement.querySelectorAll('[type=button]');
-  renderPinsInfo();
+  mapPinsElements = mapPinsElement.querySelectorAll('button:not(.map__pin--main)');
+  addPinsClickListener();
 };
 
 var onMainPinMousedown = function (evt) {
@@ -237,59 +237,56 @@ var roomsOptionsToBeEnabled = {
   '100': [NO_GEUSTS_OPTION_INDEX]
 };
 
-var setOptionsDisabledExceptDefault = function (elements) {
-  for (var i = 0; i < elements.length - 1; i++) {
-    elements[i].setAttribute('disabled', 'disabled');
-  }
-};
-
-setOptionsDisabledExceptDefault(roomsCapacityOptionsElements);
-
-var disableAllOptions = function (elements) {
-  for (var i = 0; i < elements.length; i++) {
+var disableOptions = function (elements, arrayLengths) {
+  for (var i = 0; i < arrayLengths; i++) {
     if (!elements[i].hasAttribute('disabled')) {
       elements[i].setAttribute('disabled', 'disabled');
     }
   }
 };
 
+disableOptions(roomsCapacityOptionsElements, (roomsCapacityOptionsElements.length - 1)); // disable options for 100 rooms
+
 var onRoomNumberSelectorChanged = function () {
-  disableAllOptions(roomsCapacityOptionsElements);
+  disableOptions(roomsCapacityOptionsElements, roomsCapacityOptionsElements.length);
   var roomNumberValue = roomNumberElement.value;
 
-  for (var j = 0; j < roomsOptionsToBeEnabled[roomNumberValue].length; j++) {
-    var index = roomsOptionsToBeEnabled[roomNumberValue][j];
+  for (var i = 0; i < roomsOptionsToBeEnabled[roomNumberValue].length; i++) {
+    var index = roomsOptionsToBeEnabled[roomNumberValue][i];
     roomsCapacityOptionsElements[index].removeAttribute('disabled');
   }
   roomCapacityElement.selectedIndex = roomsOptionsToBeEnabled[roomNumberValue][0];
 };
 
-var mapPopUp = [];
-var renderPinsInfo = function () {
+var pinPopUp;
+
+var addPinsClickListener = function () {
   for (var i = 0; i < mapPinsElements.length; i++) {
     addClickListener(i);
   }
 };
 
+var addEscapeListener = function (evt) {
+  if (evt.key === ESC_KEY) {
+    pinPopUp.remove();
+    document.removeEventListener('keydown', addEscapeListener);
+  }
+};
+
 var addClickListener = function (i) {
-  var removePopUpElement = function () {
-    mapPopUp[0].remove();
-  };
   mapPinsElements[i].addEventListener('click', function () {
-    if (mapPopUp.length !== 0) {
-      removePopUpElement();
+    if (pinPopUp !== undefined) {
+      pinPopUp.remove();
     }
-    document.querySelector('.map__filters-container').before(getInfoAdElement(ads[i]));
-    mapPopUp = document.querySelectorAll('.map__card');
+
+    pinPopUp = getInfoAdElement(ads[i]).children[0];
+
+    document.querySelector('.map__filters-container').before(pinPopUp);
     var mapPopUpCloseElement = document.querySelector('.popup__close');
     mapPopUpCloseElement.addEventListener('click', function () {
-      removePopUpElement();
+      pinPopUp.remove();
     });
-    window.addEventListener('keydown', function (evt) {
-      if (evt.key === ESC_KEY) {
-        removePopUpElement();
-      }
-    });
+    document.addEventListener('keydown', addEscapeListener);
   });
 };
 
@@ -316,8 +313,10 @@ var minPriceForTypes = {
 var typeElement = document.querySelector('#type');
 var priceInputElement = document.querySelector('#price');
 
-var onRoomTypeChangePrice = function () {
+var onRoomTypeChangePricePerNight = function () {
   var typeValue = typeElement.value;
   priceInputElement.min = minPriceForTypes[typeValue];
   priceInputElement.placeholder = minPriceForTypes[typeValue];
 };
+
+
